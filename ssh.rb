4bba27eb12fd3961c22b899a42902b67b34b265d
Sven Reissmann <sven@0x80.io>
2014-04-11 13:26:48 +0200

    let's start already

---
+++ ssh.rb
@@ -0,0 +1,3 @@
+rule=SSHSUCCESS: Accepted password for %user:word% from %ip:ipv4% port %port:number% %protocol:word%
+rule=SSHFAILURE: Failed password for %user:word% from %ip:ipv4% port %port:number% %protocol:word%
+rule=SSHFAILURE: Failed password for invalid user %user:word% from %ip:ipv4% port %port:number% %protocol:word%
